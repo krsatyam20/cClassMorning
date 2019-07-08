fd4ca75c683ef6be0ae5b7b23bbe4c254951fda1
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-07-08 11:34:07 +0530

    Add files via upload

---
+++ WHILEDOW.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+clrscr();
+
+int i=0;
+   while( i<10){
+     printf("\n while loop%d",i);
+    i++;
+   }
+
+int j=0;
+do{
+printf("\nDo while loop%d",j);
+j++;
+}while(j<10);
+
+
+
+getch();
+
+
+}
